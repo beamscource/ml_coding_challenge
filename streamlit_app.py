bcc4fb1e702745b6b87a9bc7a57e23a7a8899ff8
Eugen Klein <klein.eug@gmail.com>
2023-04-19 21:37:17 +0200

    Implement upload functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 import streamlit as st
 import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 st.title("Codepan Coding Challenge")
 
@@ -11,6 +12,10 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
+    m = MultipartEncoder(fields={"file": ("filename", uploaded_file, "image/jpeg")})
     st.text("Got response from backend API:")
-    req = requests.post("http://localhost:8000/score")
+    req = requests.post(
+        "http://localhost:8000/score", data=m, headers={"Content-Type": m.content_type}, timeout=8000
+    )
+    #req = requests.post("http://localhost:8000/score", files=files)
     st.text(req.json())
