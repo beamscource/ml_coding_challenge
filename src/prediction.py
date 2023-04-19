61e37c7c9d86922f8611ab456c8681f0c4bb46d7
Eugen Klein <klein.eug@gmail.com>
2023-04-19 12:16:30 +0200

    WIP: implement prediction function

--- src/prediction.py
+++ src/prediction.py
@@ -1,18 +1,80 @@
+from PIL import Image
+from torchvision import transforms
 from typing import Optional
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+def set_up_model():
+    """A function to define the model and to load trained weights.
+
+    Returns:
+        dict: a PyTorch model
+    """
+
+    class SimpleNet(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.fc1 = nn.Linear(12288, 84)
+            self.fc2 = nn.Linear(84, 50)
+            self.fc3 = nn.Linear(50,3)
+        
+        def forward(self, x):
+            x = x.view(-1, 12288)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # initialize the model and load weights
+    model = SimpleNet()
+    state_dict = torch.load("src/shoe_model.tar")
+    model.load_state_dict(state_dict)
+    model.to(device)
+
+    return model
 
 def predict(data: Optional[list] = None) -> dict:
-    """Small example function that will take some data and returns a response.
-    Right now everything is with fixed values.
+    """A function that takes an image and runs a classification on it.
 
     Args:
         data (list): prediction data
 
     Returns:
-        dict: predict response (DUMMY)
+        dict: predicted probabilities for the categories Boot, Sandal, Shoe
     """
-    return {
-        "Boot": 0.33,
-        "Sandal": 0.33,
-        "Shoe": 0.33,
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # transform input image
+    transform_function = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225] )
+        ])
+
+    image = Image.open("data/test/sandal/Sandal (948).jpg")
+    image = transform_function(image).to(device)
+    image = torch.unsqueeze(image, 0)
+
+    model = set_up_model()
+    model.eval()
+    probabilities = F.softmax(model(image), dim=1).cpu().detach().numpy()[0]
+    test = {
+        "Boot": f'{probabilities[0]:.2f}',
+        "Sandal": f'{probabilities[1]:.2f}',
+        "Shoe": f'{probabilities[2]:.2f}',
     }
+    breakpoint()
+    return {
+        "Boot": f'{probabilities[0]:.2f}',
+        "Sandal": f'{probabilities[1]:.2f}',
+        "Shoe": f'{probabilities[2]:.2f}',
+    }
+
+data = []
+predict(data)
